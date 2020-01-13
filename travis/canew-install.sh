542e7d1e8fc35f37f4dd7ff5b3996e5b7254b3b4
Lennart Spitzner <hexagoxel@hexagoxel.de>
2020-01-13 23:23:54 +0100

    reorganize env setup

--- travis/canew-install.sh
+++ travis/canew-install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$HOME/.local/bin:/opt/alex/$ALEXVER/bin:/opt/happy/$HAPPYVER/bin:$HOME/.cabal/bin:$PATH
-export JOBS=1
-
 cabal update
 cabal new-build -j$JOBS --enable-test --enable-benchmarks --dep
--- travis/canew-script.sh
+++ travis/canew-script.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$HOME/.local/bin:/opt/alex/$ALEXVER/bin:/opt/happy/$HAPPYVER/bin:$HOME/.cabal/bin:$PATH
-export JOBS=1
-
 cabal new-build -j$JOBS
