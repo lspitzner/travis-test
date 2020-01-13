02303e407e520cb67ed07e84d1198f6aab9d0505
Lennart Spitzner <hexagoxel@hexagoxel.de>
2020-01-13 23:12:43 +0100

    Add a dependency, Add install script

--- travis/canew-script.sh
+++ travis/canew-script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$HOME/.local/bin:/opt/alex/$ALEXVER/bin:/opt/happy/$HAPPYVER/bin:$HOME/.cabal/bin:$PATH
+export JOBS=1
+
+cabal new-build -j$JOBS
